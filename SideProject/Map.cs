52d44a275d25f683be0fc9075ab45272924d774f
Le Tran <lethanhbaotran.letran@gmail.com>
2022-08-18 11:12:38 +0700

    Map Editor Layout #8

--- SideProject/Map.cs
+++ SideProject/Map.cs
@@ -15,6 +15,38 @@ namespace SideProject
         public Map()
         {
             InitializeComponent();
+            initMap(); 
+        }
+
+        void initMap()
+        {
+            for(int i=0;i<30;i++)
+            {
+                for(int j=0;j<14;j++)
+                {
+                    Button btn = new Button()
+                    {
+                        Width = 50,
+                        Height = 50,
+                        Location = new Point(i * 50, j * 50),
+                    };
+
+                    MapP.Controls.Add(btn);
+                }
+            }
+        }
+
+        private void Map_Load(object sender, EventArgs e)
+        {
+            MapBox.Location = new Point(0, 0);
+
+            MapDBox.Location = new Point(0, MapBox.Height + 25);
+            MapDBox.Width = MapBox.Width;
+
+            Terrain.Location = new Point(MapBox.Width + 25, 0);
+            Terrain.Height=MapBox.Height;
+
+            fPButtonMap.Location = new Point(MapBox.Width + 25, Terrain.Height + 25);
         }
     }
 }
